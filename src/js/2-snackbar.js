eecb1b0c871fa9bebf186b3e4f3ab9390cda56c7
kim9842 <61659301+kim9842@users.noreply.github.com>
2025-05-08 18:04:04 +0300

    try

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,5 @@
 import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 const delayInput = document.querySelector('input[name="delay"]');
